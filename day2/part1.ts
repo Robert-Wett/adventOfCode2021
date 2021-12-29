8df6d238b76549758f45b7092b2c866293e92867
Rob W <rwettlaufer@mserp.com>
2021-12-29 00:09:12 -0800

    Remove logging

--- day2/part1.ts
+++ day2/part1.ts
@@ -1,38 +1,25 @@
 import { readInSubDirections } from '../helpers/day2Helper'
 
 const directions = readInSubDirections();
-/*
-down X increases your aim by X units.
-up X decreases your aim by X units.
-forward X does two things:
-It increases your horizontal position by X units.
-It increases your depth by your aim multiplied by X.
-*/
 class Submarine {
     horizontalPosition: number;
     depth: number;
-    aim: number;
 
     constructor() {
         this.horizontalPosition = 0;
         this.depth = 0;
-        this.aim = 0;
     }
 
     up(steps: number) : void {
         this.depth -= steps;
-        this.aim -= steps;
     }
 
     down(steps: number) : void {
         this.depth += steps;
-        this.aim += steps;
     }
 
     forward(steps: number) : void {
         this.horizontalPosition += steps;
-        // Handle aim
-        this.depth += this.aim * steps;
     }
 
     getCoordinateMultiplyer() : number {
@@ -46,10 +33,7 @@ class Submarine {
 
 const mySubmarine = new Submarine();
 
-console.log(mySubmarine);
-
 directions.forEach((step : any) => {
-    console.log(step);
     switch(step.direction) {
         case 'up': 
             mySubmarine.up(Number(step.steps));
@@ -58,11 +42,9 @@ directions.forEach((step : any) => {
             mySubmarine.down(Number(step.steps));
             return;
         case 'forward':
-            console.log(`Going forward ${Number(step.steps)}`)
             mySubmarine.forward(Number(step.steps));
             return;
-        default:
-            console.log('fuck you');
+        default: return;
     }
 })
 
